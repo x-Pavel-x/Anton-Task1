6def59d7d06c6b7377402cb6f4e883c049c885f2
iionov <48625130+iionov@users.noreply.github.com>
2020-01-30 22:10:13 +0300

    Add files via upload

---
+++ Task1/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+   // int nums[]=new int[] {1, 45, 34, 383, -23, 0, 34, 85, 12, 15, 22, 94};
+   // int target=64;
+    public static void main(String[] args) {
+        int[] nums=new int[] {1, 45, 34, 383, -23, 0, 34, 85, 12, 15, 22, 94};
+        int target=64;
+        int i;
+        int len=nums.length;
+        System.out.print("Given nums = [");
+        for (i=0; i<len-1; i++){
+            System.out.print(nums[i] + ", ");
+       //     if len[i]==
+        }
+        System.out.print(nums[len-1] +"], target = " +target);
+
+	//System.out.println("G: " +len +' ');
+
+
+    }
+}
