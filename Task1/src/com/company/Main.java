f35391c5f669df39e63ffd9901d0dee6dad37d2c
iionov <48625130+iionov@users.noreply.github.com>
2020-01-31 13:47:16 +0300

    Add files via upload

--- Task1/src/com/company/Main.java
+++ Task1/src/com/company/Main.java
@@ -1,4 +1,8 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
    // int nums[]=new int[] {1, 45, 34, 383, -23, 0, 34, 85, 12, 15, 22, 94};
@@ -12,11 +16,19 @@ public class Main {
         for (i=0; i<len-1; i++){
             System.out.print(nums[i] + ", ");
        //     if len[i]==
+
         }
         System.out.print(nums[len-1] +"], target = " +target);
-
+        int last = nums[nums.length-1];
+        System.out.println(" " +last);
+        Arrays.sort(nums);
+        int a=nums[0];
+        int b=nums[len-1];
+        System.out.println(a);
+        for (i=0; i<len; i++){
+            System.out.print(nums[i] + ", ");
 	//System.out.println("G: " +len +' ');
 
 
-    }
+    };
 }
